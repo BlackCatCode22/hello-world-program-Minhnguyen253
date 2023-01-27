5a003b029dbd5a4e2f8bb55b9bd1a4a70453b3a0
Minh/Mariah Nguyen <123583956+Minhnguyen253@users.noreply.github.com>
2023-01-27 14:34:43 -0800

    SampleHelloworld.py

--- HelloWorld03.py
+++ HelloWorld03.py
@@ -1,13 +1,9 @@
 # dH 1/24/23
 # HelloWorld03.py
-#
-#
-# Create a hello world program that asks the user for her name and then echos the name back with a greeting
-#
-# Reference: https://www.w3schools.com/python/python_user_input.asp
-#
+#Minh Nguyen 0920131
 # Sample code:
-user_name = input("Please enter you name: ")
+user_name = input("Please enter your first and last name: ")
 print("\n\n Hello " + user_name + " how are you today?")
+how_you = input
 
 
