9893ea66b263b69ecb8794cda1d80acf453adfba
Minh/Mariah Nguyen <ncpminh3@gmail.com>
2023-02-02 12:01:13 -0800

    this is a commit message

--- HelloWorld03.py
+++ HelloWorld03.py
@@ -1,9 +1,18 @@
-# dH 1/24/23
+# dH 2/2/23
 # HelloWorld03.py
 #Minh Nguyen 0920131
 # Sample code:
 user_name = input("Please enter your first and last name: ")
-print("\n\n Hello " + user_name + " how are you today?")
-how_you = input
+print("\n Hello " + user_name + " how are you today? ")
+how_you = input()
+print("\n Please enter your birth Month: ")
+birthMonth = input()
+birthDay = int(input("\n Please enter your birth Day: "))
+birthYear = int(input("\n Please enter your birth Year: "))
+currentYear = int(input("\n What year is this year? "))
+approxAge = currentYear - birthYear
+print(user_name, "is about", approxAge, "years old this year. \n")
+print("Thank you for using our program, " + user_name)
+
 
 
